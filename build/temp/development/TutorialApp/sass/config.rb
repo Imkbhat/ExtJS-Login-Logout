585a902d755ead77e71de4525631350608831b60
Kiran BS <kbs71190@gmail.com>
2020-09-15 12:28:10 +0530

    initial commit

---
+++ build/temp/development/TutorialApp/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
+cache_path = 'C:\Users\204653\Extjs\TutorialApp/build/.sass-cache'
---
+++ build/temp/development/TutorialApp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require C:\Users\204653\Extjs\TutorialApp\app.js
